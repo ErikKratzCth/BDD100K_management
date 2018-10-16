1b466feff01e06cdbb651d8058ce40059489e4d4
ErikKratzCth <erik.kj.kratz@gmail.com>
2018-10-16 12:55:20 +0200

    New load function

--- loadbdd100k.py
+++ loadbdd100k.py
@@ -6,54 +6,59 @@ from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from PIL import Image
 import time
 
-def load_bdd100k_attribute_spec(img_folder, labels_file, attributes_normal, attributes_outlier, num_train, num_val, num_test, outlier_frac, output_type = 'file_lists'):
-    # labels_file: full path of the JSON file with BDD100K labels.
-    # train_dir: path of directory containing training images
-    # val_dir: path of directory containing validation images
-    # test_dir: path of directory containing testing images
-    # attributes_normal: n-by-2 list of attributes to be included in the training and validation sets. 1<=n<=3. The first input in each row is the type of attribute, i.e. "weather", and the second is the value of the attribute, i.e. "rainy". Must be defined so that there is no overlap with outlier class.
-    # attributes_outlier: n-by-2 list of attributes to be included as outliers in the testing set. 1<=n<=3. Shape is same as attributes_normal. Must be defined so that there is no overlap with normal class.
+def load_bdd100k_data(img_folder, norm_file_spec, out_file_spec, num_train, num_val, num_test, outlier_frac, image_height, image_width, channels, use_file_list = True, labels_file = None, get_norm_and_out_sets = False, shuffle=False):
+    # img_folder: path of directory containing BDD100K images
+    # norm_file_spec: n-by-2 list of attributes to be included in the training and validation sets. 1<=n<=3. The first input in each row is the type of attribute, i.e. "weather", and the second is the value of the attribute, i.e. "rainy". Must be defined so that there is no overlap with outlier class.
+    # out_file_spec: n-by-2 list of attributes to be included as outliers in the testing set. 1<=n<=3. Shape is same as norm_file_spec. Must be defined so that there is no overlap with normal class.
     # num_train: number of images to be loaded into training set
     # num_val: number of images to be loaded into validation set
     # num_test: number of images to be loaded into testing set
     # outlier_frac: fraction of outliers in testing set
-    # output_type: one of 'image_data' and 'file_lists'. 'image_data' returns datasets as np.ndarrays, 'file_lists' returns a list of filenames for each set
-    
-    print('Loading json data ...', end='')
-    start_time = time.time()
-    with open(labels_file) as json_data:
-        loaded_json_data = json.load(json_data)
-    print('\rLoaded json data (%.2fs)' % (time.time()-start_time))
-    
-    start_time = time.time()
-    num_outlier_to_choose = int(math.ceil(num_test * outlier_frac))
-    num_normal_test = (num_test - num_outlier_to_choose)
-    num_normal_to_choose = num_train + num_val + num_normal_test
-    
-    print('Parsing json data ...')
-    # Find images with right attributes
-    normal_filenames, scale_factor = parse_json_data(loaded_json_data, attributes_normal, num_normal_to_choose)
-    if scale_factor >= 0:
-        num_train = int(math.ceil(scale_factor * num_train))
-        num_val = int(math.ceil(scale_factor * num_val))
-        num_normal_test = int(math.ceil(scale_factor * num_normal_test))
-        num_outlier_to_choose = int(math.ceil(scale_factor * num_outlier_to_choose))
-        print('Image sets downsized by factor %.2f' % scale_factor)
-    print('Normal file list complete (%.2fs)' % (time.time()-start_time))
-    
-    start_time = time.time()   
-    outlier_filenames, scale_factor = parse_json_data(loaded_json_data, attributes_outlier, num_outlier_to_choose)
-    if scale_factor >= 0:
-        num_train = int(math.ceil(scale_factor * num_train))
-        num_val = int(math.ceil(scale_factor * num_val))
-        num_normal_test = int(math.ceil(scale_factor * num_normal_test))
-        num_outlier_to_choose = int(math.ceil(scale_factor * num_outlier_to_choose))
-        print('Image sets downsized by factor %.2f' % scale_factor)
-    print('Outlier file list complete (%.2fs)' % (time.time()-start_time))
-    
-    
+    # image_height, image_width, channels: output data will have these dimensions
+    # use_file_list: boolean to indicate wether *_file_spec are lists of attributes (json_file will be parsed for matching files) or lists of filenames
+    # labels_file: full path of the JSON file with BDD100K labels.
+    # get_norm_and_out_sets: boolean to indicate wether to return normal and outlier data in two sets (True) or train, val and test sets (False)
+    # shuffle: boolean to indicate wether to shuffle data points. Default False => given same attributes and numbers of images, train, val and test sets are identical every time.
+
+    if not use_file_list:
+
+        print('Loading json data ...', end='')
+        start_time = time.time()
+        with open(labels_file) as json_data:
+            loaded_json_data = json.load(json_data)
+        print('\rLoaded json data (%.2fs)' % (time.time()-start_time))
+        
+        start_time = time.time()
+        num_outlier_to_choose = int(math.ceil(num_test * outlier_frac))
+        num_normal_test = (num_test - num_outlier_to_choose)
+        num_normal_to_choose = num_train + num_val + num_normal_test
+        
+        print('Parsing json data ...')
+        # Find images with right attributes
+        normal_filenames, scale_factor = parse_json_data(loaded_json_data, norm_file_spec, num_normal_to_choose)
+        if scale_factor >= 0:
+            num_train = int(math.ceil(scale_factor * num_train))
+            num_val = int(math.ceil(scale_factor * num_val))
+            num_normal_test = int(math.ceil(scale_factor * num_normal_test))
+            num_outlier_to_choose = int(math.ceil(scale_factor * num_outlier_to_choose))
+            print('Image sets downsized by factor %.2f' % scale_factor)
+        print('Normal file list complete (%.2fs)' % (time.time()-start_time))
+        
+        start_time = time.time()   
+        outlier_filenames, scale_factor = parse_json_data(loaded_json_data, out_file_spec, num_outlier_to_choose)
+        if scale_factor >= 0:
+            num_train = int(math.ceil(scale_factor * num_train))
+            num_val = int(math.ceil(scale_factor * num_val))
+            num_normal_test = int(math.ceil(scale_factor * num_normal_test))
+            num_outlier_to_choose = int(math.ceil(scale_factor * num_outlier_to_choose))
+            print('Image sets downsized by factor %.2f' % scale_factor)
+        print('Outlier file list complete (%.2fs)' % (time.time()-start_time))
+    else:
+        normal_filenames = norm_file_spec
+        outlier_filenames = out_file_spec
+
     # Assert there is no overlap between target and outlier data
-    overlap_counter = 0;
+    overlap_counter = 0
     for filename in outlier_filenames:
         if filename in normal_filenames:
             overlap_counter += 1
@@ -61,50 +66,32 @@ def load_bdd100k_attribute_spec(img_folder, labels_file, attributes_normal, attr
     if overlap_counter > 0:
         print("\nWARNING: overlap between target and outlier class: removed %d images from Outlier file list\n" % overlap_counter)
         num_outlier_to_choose -= overlap_counter
-    
-    if output_type == 'file_lists':
-        start_time = time.time()
-    
-        train_list = normal_filenames[:num_train] 
-        print("Generated train_list (%.2fs)" % (time.time()-start_time))
-    
-        start_time = time.time()
-        val_list = normal_filenames[num_train:num_train + num_val]
-        print("Generated val_list (%.2fs)" % (time.time()-start_time))
-    
-        start_time = time.time()
-        test_list = normal_filenames[num_train + num_val:] + outlier_filenames[:num_outlier_to_choose]
-        print("Generated test_list (%.2fs)" % (time.time()-start_time))
         
-        return train_list, val_list, test_list
-        
-    elif output_type == 'image_data':
-        # Specify image format
-        channels = 3
-        image_height = 720 # 180
-        image_width = 1280 # 320
+    # Specify image format
+    normal_data = np.ndarray(shape=(num_train + num_val + num_normal_test, image_height, image_width, channels), dtype=np.uint8)
+    outlier_data = np.ndarray(shape=(num_outlier_to_choose, image_height, image_width, channels), dtype=np.uint8)
 
-        normal_data = np.ndarray(shape=(num_train + num_val + num_normal_test, image_height, image_width, channels), dtype=np.uint8)
-        outlier_data = np.ndarray(shape=(num_outlier_to_choose, image_height, image_width, channels), dtype=np.uint8)
+    start_time = time.time()
+    # Load normal images
+    for i, _file in enumerate(normal_filenames):
+        img = load_img(str(img_folder / _file))  # this is a PIL image
+        img.thumbnail((image_width, image_height))
+        x = img_to_array(img)  
+        normal_data[i] = x
+    print("Normal image data loaded (%.2fs)" % (time.time()-start_time))
 
-        start_time = time.time()
-        # Load normal images
-        for i, _file in enumerate(normal_filenames):
-            img = load_img(str(img_folder / _file))  # this is a PIL image
-            img.thumbnail((image_width, image_height))
-            x = img_to_array(img)  
-            normal_data[i] = x
-        print("Normal image data loaded (%.2fs)" % (time.time()-start_time))
+    start_time = time.time()  
+    # Load outlier images
+    for i, _file in enumerate(outlier_filenames):
+        img = load_img(str(img_folder / _file))  # this is a PIL image
+        img.thumbnail((image_width, image_height))
+        x = img_to_array(img)
+        outlier_data[i] = x
+    print("Outlier image data loaded (%.2fs)" % (time.time()-start_time))
 
-        start_time = time.time()  
-        # Load outlier images
-        for i, _file in enumerate(outlier_filenames):
-            img = load_img(str(img_folder / _file))  # this is a PIL image
-            img.thumbnail((image_width, image_height))
-            x = img_to_array(img)
-            outlier_data[i] = x
-        print("Outlier image data loaded (%.2fs)" % (time.time()-start_time))
-
+    if get_norm_and_out_sets:
+        return normal_data, outlier_data
+    else:
         start_time = time.time()
 
         train_data = normal_data[:num_train] 
@@ -118,9 +105,9 @@ def load_bdd100k_attribute_spec(img_folder, labels_file, attributes_normal, attr
         test_data = np.concatenate((normal_data[num_train + num_val:], outlier_data), axis=0)
         print("Generated test_data (%.2fs)" % (time.time()-start_time))
 
-        return train_data, val_data, test_data
-    else:
-        print("Output_type argument '" + output_type + "' not supported." )
+        test_labels = np.concatenate(np.zeros((num_normal_test),dtype=int),np.ones((num_outlier_to_choose,),dtype=int))
+
+        return train_data, val_data, test_data, test_labels
 
 
 def parse_json_data(json_data, attributes_to_choose, num_to_choose):
